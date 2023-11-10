4c7c4df56cd33c387b26350370f1e1079bdd59a0
BnnQ <1v25fa94@gmail.com>
2023-11-10 18:55:51 +0200

    Added utility classes for strings and views

---
+++ src/main/java/me/bnnq/homework/Utils/Strings.java
@@ -0,0 +1,9 @@
+package me.bnnq.homework.Utils;
+
+public class Strings
+{
+    public static boolean isNullOrEmpty(String string)
+    {
+        return string == null || string.isEmpty();
+    }
+}
