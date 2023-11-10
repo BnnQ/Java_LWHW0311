f46a31d3ad01129581ee6b15f2a547063827d694
BnnQ <1v25fa94@gmail.com>
2023-11-10 18:55:00 +0200

    Initial commit

---
+++ src/main/java/me/bnnq/homework/Lwhw0311Application.java
@@ -0,0 +1,15 @@
+package me.bnnq.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lwhw0311Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Lwhw0311Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/bnnq/homework/Lwhw0311ApplicationTests.java
@@ -0,0 +1,15 @@
+package me.bnnq.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lwhw0311ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
